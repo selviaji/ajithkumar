3c44280f8748f3c985ea7ccd3ef33382d14b5511
selviaji <ajithngc@gmail.com>
2017-06-19 12:32:02 +0530

    Add files via upload

---
+++ Countrotation.java
@@ -0,0 +1,30 @@
+package guviaji;
+
+import java.util.Scanner;
+
+public class Countrotation {
+	public static void main(String[] args){
+	Scanner s=new Scanner(System.in);
+	System.out.print("Enter the number of element ");
+	int n=s.nextInt();
+	System.out.print("enter the array");
+	int a[]=new int[n];
+	for(int i=0;i<n;i++){
+		a[i]=s.nextInt();
+		
+	}System.out.print("Enter the d element to sort the array;");
+	int d=s.nextInt();
+	int m=0;
+	for(int i=0;i<n;i++){
+		if(a[i]==d){
+			 m=i;
+		}
+	}for(int i=m+1;i<n;i++){
+		System.out.print( +a[i]);
+	}for(int i=0;i<=m;i++){
+		System.out.print(+a[i]);
+	}m=m+1;
+System.out.println("\tThe k count is"+m);
+}
+}
+	
