fce681170d9dbf62c3cf4d059ad3f750b05a93f8
selviaji <ajithngc@gmail.com>
2017-07-03 10:00:53 +0530

    Add files via upload

--- Hunter10.java
+++ Hunter10.java
@@ -5,11 +5,15 @@ import java.util.Scanner;
 public class Hunter10 {
 public static void main(String[] args){
 	Scanner s=new Scanner(System.in);
+	System.out.println("enter the size a1");
 	int n=s.nextInt();
+	System.out.println("enter the element of a1");
 	int a1[]=new int[n];
 	for(int i=0;i<n;i++){
 		a1[i]=s.nextInt();
-	}int n1=s.nextInt();
+	}System.out.println("enter the size a2");
+	int n1=s.nextInt();
+	System.out.println("enter the element of a2");
 	int a2[]=new int[n1];
 	for(int i=0;i<n1;i++){
 		a2[i]=s.nextInt();
