010027e9f0b45e273b473bd43520493b6f6bd92c
selviaji <ajithngc@gmail.com>
2017-07-19 14:03:35 +0530

    Add files via upload

--- Hunter120.java
+++ Hunter120.java
@@ -1,43 +1,29 @@
 package guviaji;
 
-public class Hunter120 {
-	public static String output1;
-	public static void main(String[] args) {
-	usermethod("template");
-	System.out.println("The smallest palidrome is = "+output1);
+import java.util.Scanner;
 
-	}
-	public static void usermethod(String input1){
-		StringBuffer s=new StringBuffer(input1).reverse();
-		int count=0;
+public class Hunter120 {
+	public static void main(String[] args) {
+		Scanner s=new Scanner(System.in);
 		String n="";
-		int max=10;
-		int min=10;
-		int var1=0;
-		int var2=0;
-	for(int i=0;i<s.length();i++){count=0;
-			for(int j=0;j<s.length();j++){
-					if(input1.substring(i,i+1).equalsIgnoreCase(input1.substring(j,j+1))){
-						count++;
+		String s2="";
+		System.out.println("Enter the string");
+		String str=s.nextLine();
+		int max=str.length();
+		for(int i=0;i<str.length()-1;i++){n="";
+			for(int j=i+1;j<str.length();j++){
+				n=str.substring(i,j);
+				StringBuffer s1=new StringBuffer(n).reverse();
+				String n1=s1.toString();
+				if(n.equals(n1)&&n.length()>1){
+					if(n1.length()<max){
+						max=n.length();
+						s2=n;
 					}
-				}if(count%2==0&&count!=0){
-				if(count<max){
-					max=count;
-					var1=i;
 				}
-				}else if(count<min){
-					min=count;
-		           var2=i;
-				}
-				
-			}for(int i=0;i<max;i++){
-				n=n+input1.substring(var1,var1+1);
-			}for(int i=0;i<min;i++){
-				n=n+input1.substring(var2,var2+1);
 			}
-	output1=n;
-			
-		
+		}System.out.println(s2);
+
 	}
 
 }
