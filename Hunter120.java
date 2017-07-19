71b6b618fdc057b6f4e61ce6a6abea8c42272768
selviaji <ajithngc@gmail.com>
2017-07-19 11:31:29 +0530

    Add files via upload

---
+++ Hunter120.java
@@ -0,0 +1,45 @@
+package guviaji;
+
+public class Hunter120 {
+	public static String output1;
+	public static void main(String[] args) {
+	usermethod("template");
+	System.out.println(output1);
+
+	}
+	public static void usermethod(String input1){
+		StringBuffer s=new StringBuffer(input1).reverse();
+		int count=0;
+		String n="";
+		int max=10;
+		int min=10;
+		int var1=0;
+		int var=0;
+		int var2=0;
+	for(int i=0;i<s.length();i++){count=0;
+			for(int j=0;j<s.length();j++){
+					if(input1.substring(i,i+1).equalsIgnoreCase(input1.substring(j,j+1))){
+						count++;
+						var=i;
+					}
+				}if(count%2==0&&count!=0){
+				if(count<max){
+					max=count;
+					var1=var;
+				}
+				}else if(count<min){
+					min=count;
+		           var2=var;
+				}
+				
+			}for(int i=0;i<max;i++){
+				n=n+input1.substring(var1,var1+1);
+			}for(int i=0;i<min;i++){
+				n=n+input1.substring(var2,var2+1);
+			}
+	output1=n;
+			
+		
+	}
+
+}
